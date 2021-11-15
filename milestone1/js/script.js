cdaa4e9f69772a35d91ec85ec50f3f5a5aae2ac6
FedericoCaf <fedetheshield09@gmail.com>
2021-11-15 18:12:57 +0100

    add-dynamics-users

--- milestone1/js/script.js
+++ milestone1/js/script.js
@@ -4,6 +4,42 @@ el: '#app',
 
 data: {
 
+  users: [
+
+    {
+      name: 'Michele',
+      img: 'img/avatar_1.jpg',
+      lastMessage: 'Tutto fatto...',
+      lastDate: '',
+      isActive: true
+    },
+    {
+      name: 'Fabio',
+      img: 'img/avatar_2.jpg',
+      lastMessage: 'Mi piacerebbe ma devo andare...',
+      lastDate: '',
+      isActive: false
+
+    },
+    {
+      name: 'Samuele',
+      img: 'img/avatar_3.jpg',
+      lastMessage: 'Ah scusa!',
+      lastDate: '',
+      isActive: false
+
+    },
+    {
+      name: 'Luisa',
+      img: 'img/avatar_6.jpg',
+      lastMessage: 'Si, ma preferireri andare al ci...',
+      lastDate: '',
+      isActive: false
+    },
+
+  ],
+
+
  stringCerca: '',
 
 
